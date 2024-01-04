823491440c0fbb111f7e17100ef172b250feedb8
Henrique Fischer <104164663+fischer8@users.noreply.github.com>
2024-01-04 20:31:34 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,6 +10,7 @@ sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get -y install wget
 sudo apt-get -y install git
 sudo apt-get -y install dconf-editor
+sudo apt-get install ttf-ancient-fonts
 
 sudo apt -y install curl
 sudo apt -y install piper
