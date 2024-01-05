79651e7bc2cf5ea03f81f3413e9da0e83ac7e46e
Henrique Fischer <104164663+fischer8@users.noreply.github.com>
2024-01-05 07:31:17 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -38,7 +38,7 @@ sudo snap remove --purge firefox
 sudo rm -rf /etc/firefox/ /usr/lib/firefox/ /usr/lib/firefox-addons/
 
 sudo wget -O - https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/DroidSansMono.zip > ~/DroidSansMono.zip
-sudo unzip DroidSansMono.zip -d /usr/share/fonts/
+sudo unzip ~/DroidSansMono.zip -d /usr/share/fonts/
 
 cd ~/Downloads/ && wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb
 sudo dpkg -i ~/Downloads/steam.deb
